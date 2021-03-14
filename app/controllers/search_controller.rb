a1470506c8d53ef1688f191e9031ae28b7501f14
Saryn Mooney <smooney15@my.whitworth.edu>
2021-03-14 14:45:25 -0600

    successful JSON parse

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,9 +2,10 @@ class SearchController < ApplicationController
 
   def index
     nation = params[:nation]
-    format_nation = nation.split('_').map{ |element| element.capitalize}.join(' ')
+    format_nation = nation.split('_').map{ |name| name.capitalize}.join(' ')
     conn = Faraday.new(url: 'https://last-airbender-api.herokuapp.com/api/v1')
     response = conn.get("characters?affiliation=#{format_nation}")
+    @members = JSON.parse(response.body, symbolize_names: true)
     require 'pry'; binding.pry
   end 
 
