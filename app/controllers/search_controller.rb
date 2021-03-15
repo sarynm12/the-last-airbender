2771acffd648036c9a820d06d2dba8cfccdc6e99
Saryn Mooney <smooney15@my.whitworth.edu>
2021-03-15 11:07:27 -0600

    add in page and count params in api call

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,7 +4,7 @@ class SearchController < ApplicationController
     nation = params[:nation]
     format_nation = nation.split('_').map{ |name| name.capitalize}.join(' ')
     conn = Faraday.new(url: 'https://last-airbender-api.herokuapp.com/api/v1')
-    response = conn.get("characters?affiliation=#{format_nation}")
+    response = conn.get("characters?perPage=100?page=1&affiliation=#{format_nation}")
     @members = JSON.parse(response.body, symbolize_names: true)
     require 'pry'; binding.pry
   end 
