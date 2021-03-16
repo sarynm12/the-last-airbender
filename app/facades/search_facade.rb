d77f303ad8fe4e8540a37256b9381650d71f78e3
Saryn Mooney <smooney15@my.whitworth.edu>
2021-03-15 21:41:06 -0600

    move api call to facade

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -1,3 +1,11 @@
 class SearchFacade
 
+  def self.find_members(nation)
+    #nation = params[:nation]
+    format_nation = nation.split('_').map{ |name| name.capitalize}.join(' ')
+    conn = Faraday.new(url: 'https://last-airbender-api.herokuapp.com/api/v1')
+    response = conn.get("characters?perPage=100?page=1&affiliation=#{format_nation}")
+    JSON.parse(response.body, symbolize_names: true)
+  end 
+
 end 
