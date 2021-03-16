fa8669dad6687b7e17a53b3d973761d2b7927ba4
Saryn Mooney <smooney15@my.whitworth.edu>
2021-03-15 21:54:21 -0600

    move api call to service

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -1,11 +1,15 @@
 class SearchFacade
 
   def self.find_members(nation)
-    #nation = params[:nation]
-    format_nation = nation.split('_').map{ |name| name.capitalize}.join(' ')
-    conn = Faraday.new(url: 'https://last-airbender-api.herokuapp.com/api/v1')
-    response = conn.get("characters?perPage=100?page=1&affiliation=#{format_nation}")
-    JSON.parse(response.body, symbolize_names: true)
+    data = SearchService.nation_call(nation)
+    @members = data.first(25).map do |member_attrs|
+      Member.new(member_attrs)
+    end 
+      
+    # format_nation = nation.split('_').map{ |name| name.capitalize}.join(' ')
+    # conn = Faraday.new(url: 'https://last-airbender-api.herokuapp.com/api/v1')
+    # response = conn.get("characters?perPage=100?page=1&affiliation=#{format_nation}")
+    # JSON.parse(response.body, symbolize_names: true)
   end 
 
 end 
