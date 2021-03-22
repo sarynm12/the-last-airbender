ac795b8103db9e0fb754a2f896748b9e0ebc82f8
Saryn Mooney <smooney15@my.whitworth.edu>
2021-03-22 16:20:52 -0600

    complete search facade spec

--- spec/facades/search_facade_spec.rb
+++ spec/facades/search_facade_spec.rb
@@ -1,1 +1,18 @@
 require 'rails_helper'
+
+RSpec.describe 'search facade' do 
+  it 'should call on the facade and retrieve search results' do 
+    nation = 'Fire Nation'
+    data = SearchFacade.find_members(nation)
+    
+    expect(data).to be_an(Array)
+
+    first_nation = data[0]
+    expect(first_nation).to be_a(Member)
+    expect(first_nation.affiliation).to eq("Fire Nation Navy")
+    expect(first_nation.allies).to eq(["Ozai"])
+    expect(first_nation.enemies).to eq(["Earth Kingdom"])
+    expect(first_nation.name).to eq("Chan (Fire Nation admiral)")
+    expect(first_nation.photo).to eq(nil)
+  end 
+end 
